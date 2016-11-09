160009bd9408b9dec77a5eb830b200a47b1b46ca
Kevin Aguilar <kevin.16.mn@gmail.com>
2016-11-08 21:13:15 -0400

    Receive Neighbours Data: se recive los datos de los servidores a quienes atacar y los datos de los arduinos vecinos

--- arduino.js
+++ arduino.js
@@ -1,11 +1,18 @@
-var port = process.argv.slice(2)[0];
-var id = process.argv.slice(2)[1]
+var id = process.argv[2];
+//console.log(id);
+var ip = process.argv[3]
+//console.log(ip)
+var port = process.argv[4];
+//console.log(port);
+var number_process = process.argv[5]
+
 var delay = 1000;
 
 var express = require('express');  
 var app = express();
 var server = require('http').Server(app);  
 var request = require('request');
+var io = require('socket.io-client')('http://localhost:3000');
 
 /*server.listen(port, function() {  
     console.log('Servidor corriendo en http://localhost:' + port);
@@ -13,15 +20,26 @@ var request = require('request');
 
 setInterval(post_Request,delay);
 
+io.on("arduinos",function(data){
+    console.log(data);
+    //Start Lead Election
+    var lead = getLead(data.arduinos);
+    console.log(lead);
+});
+
 function post_Request(){
     request({
         url: 'http://localhost:3000',
         method: 'POST',
         json: {"id" : id,
+                "number_process": number_process,
                 "datetime" : getDateTime(),
                 "data": { 
                     "sensor1": random(0,20,5), 
-                    "sensor2": random(0,20,5)}
+                    "sensor2": random(0,20,5)
+                },
+                "ip": ip,
+                "port": port
             }
         }, function(error, response, body){
             if(body != null){        
@@ -57,4 +75,16 @@ function getDateTime() {
     day = (day < 10 ? "0" : "") + day;
 
     return year + "-" + month + "-" + day + ":" + hour + ":" + min + ":" + sec;
+}
+
+function getLead(arduinos){
+    var max = 0;
+    var index = 0; 
+    for (var i = arduinos.length - 1; i >= 0; i--) {
+        if(arduinos[i].number_process > max){
+            index = i;
+            max = arduinos[i].number_process;
+        }
+    }
+    return arduinos[index];
 }
